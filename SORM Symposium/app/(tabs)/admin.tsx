8acb9b6dc0a3c23442b14a9683498560b15d94db
Mark Evola <markevola24@gmail.com>
2025-05-20 15:54:40 -0500

    SCA-13 add admin tab

---
+++ SORM Symposium/app/(tabs)/admin.tsx
@@ -0,0 +1,6 @@
+import { ThemedView } from "@/components/ThemedView";
+import { ThemedText } from "@/components/ThemedText";
+
+export default function AdminPage() {
+  return <ThemedText>This is the admin page.</ThemedText>;
+}
