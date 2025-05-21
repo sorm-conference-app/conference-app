d7ee50b755c590678d5040d206f3c3a7f90e1bdc
Mark Evola <markevola24@gmail.com>
2025-05-21 12:11:45 -0500

    SCA-13 Re-implement admin page

---
+++ SORM Symposium/app/(tabs)/_layout.tsx
@@ -0,0 +1,32 @@
+import { Tabs } from "expo-router";
+import { IconSymbol } from "@/components/ui/IconSymbol";
+import { useColorScheme } from "react-native";
+import { useTabBarStyle } from "@/components/ui/TabBarBackground";
+
+export default function RootLayout() {
+  const colorScheme = useColorScheme() ?? "light";
+  const screenOptions = useTabBarStyle(colorScheme);
+
+  return (
+    <Tabs screenOptions={screenOptions}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol name="house.fill" color={color} size={28} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="admin"
+        options={{
+          title: "Admin",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol name="person.fill" color={color} size={28} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
---
+++ SORM Symposium/app/(tabs)/admin.tsx
@@ -0,0 +1,5 @@
+import { View } from "react-native";
+
+export default function Admin() {
+  return <View>This is the admin page.</View>;
+}
