a06403c9e2bc1562301d5c6ca895e965e63606e5
Mark Evola <markevola24@gmail.com>
2025-05-21 12:19:16 -0500

    SCA-13 basic pin functionality

--- SORM Symposium/app/(tabs)/admin.tsx
+++ SORM Symposium/app/(tabs)/admin.tsx
@@ -1,5 +1,35 @@
-import { View } from "react-native";
+import { useState } from "react";
+import { View, TextInput, Text, Button } from "react-native";
+
+const TEMPORARY_PIN = "1234";
 
 export default function Admin() {
-  return <View>This is the admin page.</View>;
+  const [pin, setPin] = useState<string>("");
+
+  function handlePinChange(text: string) {
+    setPin(text);
+  }
+
+  function resetPin() {
+    setPin("");
+  }
+
+  if (pin === TEMPORARY_PIN) {
+    return (
+      <View>
+        <Text>Welcome to the admin panel!</Text>
+        <Button onPress={resetPin} title="Go back" />
+      </View>
+    );
+  }
+
+  return (
+    <View>
+      <TextInput
+        value={pin}
+        onChangeText={handlePinChange}
+        placeholder="Enter PIN"
+      />
+    </View>
+  );
 }
