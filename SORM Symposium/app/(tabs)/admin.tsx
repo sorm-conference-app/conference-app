cd6c4b78a6454f6ea814023052b8a418a28c5dd0
JamesDCowley <wuz7@txstate.edu>
2025-05-22 08:13:22 -0500

    SCA-16 PIN number input and admin panel contents now wrapped inside of SafeAreaView, fixes issue

--- SORM Symposium/app/(tabs)/admin.tsx
+++ SORM Symposium/app/(tabs)/admin.tsx
@@ -2,7 +2,7 @@ import { ThemedView } from "@/components/ThemedView";
 import { ThemedText } from "@/components/ThemedText";
 import ThemedTextInput from "@/components/ThemedTextInput";
 import { useState } from "react";
-import { Button } from "react-native";
+import { Button, SafeAreaView } from "react-native";
 
 const TEMPORARY_PIN = "1234";
 
@@ -19,20 +19,24 @@ export default function Admin() {
 
   if (pin === TEMPORARY_PIN) {
     return (
-      <ThemedView>
-        <ThemedText>Welcome to the admin panel!</ThemedText>
-        <Button onPress={resetPin} title="Go back" />
-      </ThemedView>
+      <SafeAreaView>
+        <ThemedView>
+          <ThemedText>Welcome to the admin panel!</ThemedText>
+          <Button onPress={resetPin} title="Go back" />
+        </ThemedView>
+      </SafeAreaView>
     );
   }
 
   return (
-    <ThemedView>
-      <ThemedTextInput
-        value={pin}
-        onChangeText={handlePinChange}
-        placeholder="Enter PIN"
-      />
-    </ThemedView>
+    <SafeAreaView>
+      <ThemedView>
+        <ThemedTextInput
+          value={pin}
+          onChangeText={handlePinChange}
+          placeholder="Enter PIN"
+        />
+      </ThemedView>
+    </SafeAreaView>
   );
 }
