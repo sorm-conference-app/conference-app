b6bf84ca98a56c1a6a0cfc7a880dfb979e6f5e0f
0neye <sidusmatter@gmail.com>
2025-05-23 16:16:03 -0500

    Moved keys to .env

--- SORM Symposium/constants/supabase.ts
+++ SORM Symposium/constants/supabase.ts
@@ -1,8 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-// Replace these with your actual Supabase project credentials
-const SUPABASE_URL = 'https://izlxjqgiauljmlqtzrto.supabase.co'
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml6bHhqcWdpYXVsam1scXR6cnRvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc2OTMyNjgsImV4cCI6MjA2MzI2OTI2OH0.LK7OykPBKcgR5Hczk8nIY8Z6LVYgfOXWAV7RRGOZ4EI'
+const SUPABASE_URL = process.env.EXPO_PUBLIC_SUPABASE_URL!
+const SUPABASE_ANON_KEY = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!
 
 /**
  * The Supabase client instance for making API calls to your backend.
