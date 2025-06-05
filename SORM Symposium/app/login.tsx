2683ed44240dd7ec5446bb8bb1add5527403c59e
Mark Evola <markevola24@gmail.com>
2025-06-04 19:24:13 -0500

    sca-33: styles and email validation

--- SORM Symposium/app/login.tsx
+++ SORM Symposium/app/login.tsx
@@ -1,6 +1,6 @@
 import { ThemedView } from "@/components/ThemedView";
 import { ThemedText } from "@/components/ThemedText";
-import { Button } from "react-native";
+import { Button, StyleSheet } from "react-native";
 import { router } from "expo-router";
 import { useState } from "react";
 import ThemedTextInput from "@/components/ThemedTextInput";
@@ -14,6 +14,9 @@ export default function Login() {
     email: "",
     password: "",
   });
+  const validEmail = /^[A-Za-z0-9]+@[A-Za-z0-9-]+\.[A-Za-z]{2,}$/.test(
+    login.email,
+  );
 
   const updateLoginDetails = (field: "email" | "password", value: string) => {
     setLogin((prev) => ({
@@ -31,7 +34,7 @@ export default function Login() {
   };
 
   const initialJSX = (
-    <ThemedView>
+    <ThemedView style={styles.container}>
       <ThemedText type="title">Welcome!</ThemedText>
       <ThemedText>
         We&apos;re excited you&apos;re here. To continue, please identify who
@@ -52,27 +55,48 @@ export default function Login() {
   }
 
   return (
-    <ThemedView>
+    <ThemedView style={styles.container}>
       <ThemedText type="title">Welcome!</ThemedText>
       <ThemedText>Sign in to continue.</ThemedText>
 
-      <ThemedText>Email</ThemedText>
-      <ThemedTextInput
-        value={login.email}
-        textContentType="emailAddress"
-        onChangeText={(txt) => updateLoginDetails("email", txt)}
-      />
+      <ThemedView style={styles.inputContainer}>
+        <ThemedText>Email</ThemedText>
+        <ThemedTextInput
+          value={login.email}
+          textContentType="emailAddress"
+          onChangeText={(txt) => updateLoginDetails("email", txt)}
+        />
+        {login.email.length > 0 && !validEmail && (
+          <ThemedText style={styles.invalid}>Not a valid email.</ThemedText>
+        )}
+      </ThemedView>
 
-      <ThemedText>Password</ThemedText>
-      <ThemedTextInput
-        value={login.password}
-        textContentType="password"
-        secureTextEntry
-        onChangeText={(txt) => updateLoginDetails("password", txt)}
-      />
+      <ThemedView style={styles.inputContainer}>
+        <ThemedText>Password</ThemedText>
+        <ThemedTextInput
+          value={login.password}
+          textContentType="password"
+          secureTextEntry
+          onChangeText={(txt) => updateLoginDetails("password", txt)}
+        />
+      </ThemedView>
 
-      <Button title="Sign In" />
+      <Button title="Sign In" disabled={!validEmail} />
       <Button title="Back" onPress={() => setShowLoginScreen(false)} />
     </ThemedView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 25,
+    height: "100%",
+  },
+  inputContainer: {
+    marginTop: 5,
+    marginBottom: 5,
+  },
+  invalid: {
+    color: "red",
+  },
+});
